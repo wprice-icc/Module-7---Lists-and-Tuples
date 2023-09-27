45c37238e59ea2ef6b19a4a2c657413a4cd5b406
wprice-icc <142839448+wprice-icc@users.noreply.github.com>
2023-09-27 18:45:51 -0500

    Create m7.py

---
+++ m7.py
@@ -0,0 +1,46 @@
+1. Last Three Elements
+
+Description: Extract the last three elements from a list.
+Variables: input_list
+
+2. Add Tuples
+
+Description: Given two tuples of integers, return a new tuple with the sum of their elements.
+Variables: tuple1, tuple2
+
+
+3. Slice Every Nth Element
+
+Description: Return every nth element from a list.
+Variables: elements_list, n
+
+4. Populate List
+
+Description: Create a list with the numbers from start to end inclusive, and then remove (pop) the last element.
+Variables: start, end
+
+
+5. Shallow Copy
+
+Description: Create a shallow copy of a list.
+Variables: original_list
+
+
+6. Filter Even Numbers
+
+Description: Use list comprehension to get even numbers from a list.
+Variables: nums
+
+
+7. 2D List Sum
+
+Description: Given a 2D list (matrix), find the sum of all its elements.
+Variables: matrix_2d
+
+
+8. Convert to Tuple
+
+Description: Convert a given list into a tuple.
+Variables: elements
+
+
